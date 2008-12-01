5ce09ee28bc904966616d6d8abbd3609b45f0928
tpackers <tpackers@9efcf954-b67b-11dd-a067-9b1d96bc3259>
2008-12-01 20:28:23 +0000

    Adding new classes from Oliver's code.

---
+++ trunk/src/utilities/BoundingBox.java
@@ -0,0 +1,12 @@
+package utilities;
+public class BoundingBox {
+	public int minX,maxX,minY,maxY;
+	public BoundingBox(int minX, int maxX, int minY, int maxY) {
+		this.minX = minX;
+		this.maxX= maxX;
+		this.minY= minY;
+		this.maxY=maxY;
+		
+	}
+
+}
