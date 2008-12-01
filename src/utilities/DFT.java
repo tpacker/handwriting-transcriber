fd24f0d0e2dce6230aa132d8c123a52e63a2b000
tpackers <tpackers@9efcf954-b67b-11dd-a067-9b1d96bc3259>
2008-12-01 20:28:23 +0000

    Adding new classes from Oliver's code.

---
+++ src/utilities/DFT.java
@@ -0,0 +1,61 @@
+package utilities;
+public class DFT {
+	
+	public static final int RECTANGULAR = 0;
+	public static final int HANN = 1;
+	public static final int HAMMING = 2;
+	public static final int BLACKMANN = 3;
+	
+	public static final double[] forwardMagnitude(double[] input) {
+		int N = input.length;
+		double[] mag = new double[N];
+		double[] c = new double[N];
+		double[] s = new double[N];
+		double twoPi = 2*Math.PI;
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				c[i] += input[j]*Math.cos(i*j*twoPi/N);
+				s[i] -= input[j]*Math.sin(i*j*twoPi/N);
+			}
+			c[i]/=N;
+			s[i]/=N;
+			
+			mag[i]=Math.sqrt(c[i]*c[i]+s[i]*s[i]);
+		}
+		
+		return mag;
+	}
+	
+	public static final double[] sineValues(double[] input) {
+		int N = input.length;
+		double[] s = new double[N];
+		double twoPi = 2*Math.PI;
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				s[i] -= input[j]*Math.sin(i*j*twoPi/N);
+			}
+			s[i]/=N;
+			
+		}
+		
+		return s;
+	}
+	
+	public static final double[] cosineValues(double[] input) {
+		int N = input.length;
+		double[] c = new double[N];
+		double twoPi = 2*Math.PI;
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				c[i] += input[j]*Math.cos(i*j*twoPi/N);
+			}
+			c[i]/=N;
+			
+		}
+		
+		return c;
+	}
+}
