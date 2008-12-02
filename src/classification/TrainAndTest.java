70e4dd1c89256a254a7b3494088b9d8a078b9704
tpackers <tpackers@9efcf954-b67b-11dd-a067-9b1d96bc3259>
2008-12-02 00:28:05 +0000

    Staring to code the training and probability model portion.

--- src/classification/TrainAndTest.java
+++ src/classification/TrainAndTest.java
@@ -11,27 +11,54 @@ import jigl.image.GrayImage;
  */
 public class TrainAndTest
 {
+	public static final int testSetSize = 10;  
+	
+	
 	/**
 	 * Train and classify.
 	 */
 	public static void main(String[] args)
 	{
 		// Create row set.
-		ArrayList rowSet = RowSetMaker.MakeRowSet();
+		ArrayList<RecordRow> rowSet = RowSetMaker.MakeRowSet();
 		
 		// Split into train and test sets.
+		ArrayList<RecordRow> trainSet = new ArrayList<RecordRow>();
+		ArrayList<RecordRow> testSet = new ArrayList<RecordRow>();
+		for (int rowPos = 0; rowPos < rowSet.size(); rowPos++)
+		{
+			if (rowPos < rowSet.size() - testSetSize)
+			{
+				trainSet.add(rowSet.get(rowPos));
+			}
+			else
+			{
+				testSet.add(rowSet.get(rowPos));
+			}
+		}
+		
+		// Train.
+		ProbabilityModel probabilityModel = train(trainSet);		
+		
+		// Test.
+		test(probabilityModel, testSet);		
 		
 		System.out.println("Done.");
 	}
 	
-	private static void train()
+	
+	private static ProbabilityModel train(ArrayList<RecordRow> trainSet)
 	{
+		ProbabilityModel probabilityModel = new ProbabilityModel();
+		
 		// Compute state transition probabilities.
+		probabilityModel.computeStateTransitionProbs(trainSet);
 		
-		
+		return probabilityModel;
 	}
 	
-	private static void test()
+	
+	private static void test(ProbabilityModel probabilityModel, ArrayList<RecordRow> testSet)
 	{
 		
 	}
